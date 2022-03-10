f78fbef82e764b0a4ef8c68829ec185075402c39
Felipe Lima <mrfelipelima@gmail.com>
2022-03-10 13:15:04 -0300

    refatorando foodfy done

--- public/scripts.js
+++ public/scripts.js
@@ -3,16 +3,6 @@ const cards = document.querySelectorAll('.card-receita');
 
 for (let card of cards) {
     card.addEventListener('click', function () {
-        const recipeId = card.getAttribute('id');
-        const recipeTitle = card.querySelector('.title-receita').textContent
-        const recipeAuthor = card.querySelector('.author-receita').textContent
-        modalOverlay.classList.add('active');
-        modalOverlay.querySelector('.recipe-img').src = `/assets/${recipeId}.png`
-        modalOverlay.querySelector('.modal-recipe-title').textContent = recipeTitle;
-        modalOverlay.querySelector('.modal-recipe-author').textContent = recipeAuthor;
+        window.location.href = `/recipe/${recipeId}`
     })
 }
-
-document.querySelector('.close-modal').addEventListener('click', function () {
-    modalOverlay.classList.remove('active');
-});
--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const nunjucks = require('nunjucks');
 
 
 const app = express();
+const data = require('./data');
 
 app.use(express.static('public'));
 
-app.set('view engine', 'html')
+app.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-    express: app
+    express: app,
+    autoescape: false,
+    noCache: true
 })
 
 app.get('/', function(req, res) {
-    res.render('index')
+    res.render('home', {items: data})
 })
 
 app.get('/about', function(req, res) {
@@ -21,7 +24,13 @@ app.get('/about', function(req, res) {
 })
 
 app.get('/recipes', function(req, res) {
-    res.render('recipes')
+    res.render('recipes', {items: data});
+})
+
+app.get('/recipe/:id', function(req, res) {
+    const id = req.params.id
+    const recipe = data[id]
+    res.render('recipev2', {recipe})
 })
 
 app.listen(process.env.PORT || 5000, function () {
